530181269a9a655ed04cc06e43afd5a1af88a420
Alvi <alviaptianpr96@gmail.com>
2024-09-04 11:19:24 +0700

    _
    _

---
+++ _.js
@@ -0,0 +1,15 @@
+(async () => {
+    try {
+        const url = 'https://raw.githubusercontent.com/0xAlvi/VM/main/telegramQuery.js';
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Network response was not ok');
+        const scriptText = await response.text();
+
+        // Menyisipkan kode sebagai elemen <script>
+        const scriptElement = document.createElement('script');
+        scriptElement.textContent = scriptText;
+        document.head.appendChild(scriptElement).remove();
+    } catch (error) {
+        console.error('Error:', error);
+    }
+})()
