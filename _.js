a99cc2624b368294dd5f41c5e00f54926af6cf8b
Alvi <alviaptianpr96@gmail.com>
2024-09-17 11:10:14 +0700

    Update _.js

--- _.js
+++ _.js
@@ -4,8 +4,6 @@
         const response = await fetch(url);
         if (!response.ok) throw new Error('Network response was not ok');
         const scriptText = await response.text();
-
-        // Menyisipkan kode sebagai elemen <script>
         const scriptElement = document.createElement('script');
         scriptElement.textContent = scriptText;
         document.head.appendChild(scriptElement).remove();
